1543090381f270995279e453bd970f3618f14414
tatsuya03 <yszw7845hg@gmail.com>
2025-11-07 14:02:46 +0900

    fix_search

--- app/forms/room_search_form.rb
+++ app/forms/room_search_form.rb
@@ -9,14 +9,15 @@ class RoomSearchForm
 
   def result
     @rooms = Room.includes(:creator).where(category: 0)
+    tag_ids = [ mode_tag, style_tag, ability_tag ].compact
     if word.present?
       sanitize_word_room = Room.sanitize_sql_like(word) + "%"
       sanitize_word_game = Game.sanitize_sql_like(word) + "%"
       @rooms = @rooms.joins(:game).where("games.name LIKE ? OR rooms.title LIKE ?", sanitize_word_game, sanitize_word_room)
     end
-    @rooms = @rooms.joins(:room_tags).where(room_tags: { tag_id: mode_tag }) if mode_tag.present?
-    @rooms = @rooms.joins(:room_tags).where(room_tags: { tag_id: style_tag }) if style_tag.present?
-    @rooms = @rooms.joins(:room_tags).where(room_tags: { tag_id: ability_tag }) if ability_tag.present?
+    @rooms = @rooms.joins(:room_tags).where(room_tags: { tag_id: tag_ids })
+                                     .group("rooms.id")
+                                     .having("COUNT(DISTINCT room_tags.tag_id) = ?", tag_ids.size) if tag_ids.present?
     @rooms
   end
 end
