9f00ae29b5e0379d669e8674d4fabf866ceb9b81
tatsuya03 <yszw7845hg@gmail.com>
2025-11-15 12:08:17 +0900

    google_verify

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,6 +56,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def build_resource(hash = {})
+    hash[:uid] = User.create_unique_string
+    super
+  end
+
+  def update_resource(resource, params)
+    return super if params['password'].present?
+
+    resource.update_without_password(params.except('current_password'))
+  end
+
   # DELETE /resource
   # def destroy
   #   super
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_one :profile, dependent: :destroy
   has_many :rooms, class_name: "Room", foreign_key: "creator_id", dependent: :destroy
@@ -17,11 +18,36 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :first_name, presence: true, length: { maximum: 255 }
-  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :first_name,  length: { maximum: 255 }
+  validates :last_name,  length: { maximum: 255 }
+  validate :first_name_cannot_be_nil
+  validate :last_name_cannot_be_nil
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
   validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   enum role: { general: 0, admin: 1 }
+
+  def first_name_cannot_be_nil
+    errors.add(:first_name, "can't be nil") if first_name.nil?
+  end
+
+  def last_name_cannot_be_nil
+    errors.add(:last_name, "can't be nil") if last_name.nil?
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      names = auth.info.name.split(" ", 2)
+      user.last_name = names[0]
+      user.first_name = names[1] || ""
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.password_confirmation = user.password
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end
