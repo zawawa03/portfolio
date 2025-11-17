8139e36409d242d0dbe6cd027d69d18fd48b8ba9
tatsuya03 <yszw7845hg@gmail.com>
2025-11-17 20:59:18 +0900

    fix_channel

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,8 +1,8 @@
 import consumer from "./consumer"
 
 document.addEventListener("turbo:load",() =>{
-  const notification_field = document.querySelector('#notification_field')
-  if (notification_field) {
+  const notification_user = document.querySelector('#notification_user')
+  if (notification_user) {
     consumer.subscriptions.create({channel: "NotificationChannel", user_id: document.querySelector('#notification_user').dataset.id}, {
       connected() {
         console.log('notification_channel接続');
@@ -13,12 +13,10 @@ document.addEventListener("turbo:load",() =>{
       },
 
       received(data) {
-        if (!notification_field) return
-        notification_field.remove();
-        const notification_user = document.querySelector('#notification_user')
-        if (!notification_user) return
-        notification_user.insertAdjacentHTML('afterbegin', data['notification'])
+        const old_notification_field = document.querySelector('#notification_field')
+        old_notification_field.remove();
+        notification_user.insertAdjacentHTML('beforeend', data['notification'])
       }
     });
-  };  
-});  
+  };
+});
