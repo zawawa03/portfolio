eab03ec6abdb2c7b40129e104a403be4f7f94cd7
tatsuya03 <yszw7845hg@gmail.com>
2025-11-30 20:52:31 +0900

    fix_search_form

--- app/forms/board_search_form.rb
+++ app/forms/board_search_form.rb
@@ -8,8 +8,8 @@ class BoardSearchForm
   def result
     @boards = Board.includes(:creator).order(created_at: :DESC)
     if word.present?
-      sanitize_title_board = Room.sanitize_sql_like(word) + "%"
-      sanitize_title_game = Game.sanitize_sql_like(word) + "%"
+      sanitize_title_board = "%#{Room.sanitize_sql_like(word)}%"
+      sanitize_title_game = "%#{Game.sanitize_sql_like(word)}%"
       @boards = @boards.joins(:game).where("boards.title LIKE ? OR games.name LIKE ?", sanitize_title_board, sanitize_title_game)
     end
     if tag_id.present?
