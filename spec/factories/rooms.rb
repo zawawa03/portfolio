1186c26ebb134d61bbe4c4cca8d28ae05dba179b
tatsuya03 <yszw7845hg@gmail.com>
2025-12-27 11:50:06 +0900

    room_system_spec

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :room do
     association :creator, factory: :user
     association :game
-    title { "テストゲーム" }
+    title { "テストゲーム募集" }
     body { "よろしく" }
     people { 5 }
     category { 0 }
--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Rooms", type: :system do
     let!(:tag0) { FactoryBot.create(:tag) }
     let!(:tag1) { FactoryBot.create(:tag, :category_1) }
     let!(:tag2) { FactoryBot.create(:tag, :category_2) }
+    let!(:user_room) { FactoryBot.create(:user_room, user: user, room: room) }
 
     context "未ログイン時" do
       it "未ログイン時は新規作成画面へアクセスできない" do
@@ -29,7 +30,7 @@ RSpec.describe "Rooms", type: :system do
 
       it "一覧に募集が表示されている" do
         visit rooms_path
-        expect(page).to have_content("テストゲーム")
+        expect(page).to have_content("テストゲーム募集")
       end
     end
 
@@ -103,6 +104,103 @@ RSpec.describe "Rooms", type: :system do
         create_room(room)
         visit rooms_path
         expect(page).to have_content("テストルーム")
+      end
+    end
+
+    context "チャットページの機能" do #メッセージ送信系はmessageのテストでやる
+      before { login(user) }
+
+      it "退出ボタンを押すと退出できる" do
+        create_room(room)
+        click_on "退出"
+        expect{
+          expect(page.accept_confirm).to eq("退出しますか？参加者がいなくなると自動的に募集は削除されます")
+          expect(page).to have_content("退出しました")
+        }
+      end
+
+      it "退出が最後の一人ならその募集は削除される" do
+        room = FactoryBot.build(:room, creator: user, game: game, title: "削除するルーム")
+        create_room(room)
+        click_on "退出"
+        accept_confirm do
+          expect(page.driver.browser.switch_to.alert.text).to eq("退出しますか？参加者がいなくなると自動的に募集は削除されます")
+        end
+        expect(page).to have_content("退出しました")
+        expect(page).not_to have_content("削除するルーム")
+      end
+
+      it "編集ボタンから編集ページへ遷移できる" do
+        create_room(room)
+        click_on "編集"
+        expect(page).to have_content("ルーム編集")
+      end
+    end
+
+    context "編集機能" do
+      before { login(user) }
+
+      it "募集を編集できる" do
+        visit edit_room_path(room)
+        fill_in "タイトル", with: "編集したタイトル"
+        click_on "更新"
+        expect(page).to have_content("募集を編集しました")
+      end
+
+      it "編集でタイトルを入力しないとエラー" do
+        visit edit_room_path(room)
+        fill_in "タイトル", with: nil
+        click_on "更新"
+        expect(page).to have_content("募集を編集できませんでした")
+        expect(page).to have_content("タイトルを入力してください")
+      end
+      it "編集するとチャットページへ遷移" do
+        visit edit_room_path(room)
+        fill_in "タイトル", with: "編集したタイトル"
+        click_on "更新"
+        expect(page).to have_content("チャットルーム")
+      end
+    end
+
+    context "詳細機能" do # 承認系はpermitのテストでやる
+      before { login(user) }
+
+      it "一覧ページからリンクで詳細画面へアクセスできる" do
+        user1 = FactoryBot.create(:user, last_name: "吉田", first_name: "沙織")
+        profile1 = FactoryBot.create(:profile, nickname: "よし", user: user1)
+        room1 = FactoryBot.create(:room, title: "よしの募集", creator: user1, game: game)
+        visit rooms_path
+        click_on "よしの募集"
+        expect(page).to have_content("募集詳細")
+        expect(page).to have_content("よしの募集")
+      end
+
+      it "募集に参加している場合はチャットルームへ遷移する" do
+        visit rooms_path
+        click_on("テストゲーム募集", match: :first)
+        expect(page).to have_content("チャットルーム")
+      end
+    end
+    
+    context "検索機能" do
+      it "キーワード検索ができる" do
+        user1 = FactoryBot.create(:user, last_name: "吉田", first_name: "沙織")
+        profile1 = FactoryBot.create(:profile, nickname: "よし", user: user1)
+        room1 = FactoryBot.create(:room, title: "よしの募集", creator: user1, game: game)
+        visit rooms_path
+        fill_in "q[word]", with: "よし"
+        expect(page).to have_content("よしの募集")
+      end
+
+      it "タグ検索ができる" do
+        user1 = FactoryBot.create(:user, last_name: "吉田", first_name: "沙織")
+        profile1 = FactoryBot.create(:profile, nickname: "よし", user: user1)
+        room1 = FactoryBot.create(:room, title: "よしの募集", creator: user1, game: game)
+        tag4 = FactoryBot.create(:tag, name: "セカンドモードタグ", category: 0)
+        room_tag = FactoryBot.create(:room_tag, tag: tag4, room: room1)
+        visit rooms_path
+        select "セカンドモードタグ", from: "q[mode_tag]"
+        expect(page).to have_content("よしの募集")
       end
     end
   end
