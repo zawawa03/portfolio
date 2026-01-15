5b9d46e127e4c4355fc9bc9f668b53ed43793733
tatsuya03 <yszw7845hg@gmail.com>
2026-01-15 15:02:20 +0900

    Rspec_test

--- app/decorators/description_decorator.rb
+++ app/decorators/description_decorator.rb
@@ -1,6 +1,5 @@
 class DescriptionDecorator < Draper::Decorator
   delegate_all
-
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
@@ -9,5 +8,4 @@ class DescriptionDecorator < Draper::Decorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
