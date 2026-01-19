fce0d55d3c7999c66f0b652b57e9ea65761b6c97
tatsuya03 <yszw7845hg@gmail.com>
2026-01-19 14:08:07 +0900

    fix_rubocop

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -2,11 +2,5 @@ class DescriptionsController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :profile_check
 
-  def show
-    begin
-      raise "Rollbar test error"
-    rescue => e
-      Rollbar.error(e)
-    end
-  end
+  def show; end
 end
